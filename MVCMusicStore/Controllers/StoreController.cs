f70a45ae2685c380dbb8bdfb18b3b2949a7729c8
aditya365 <aditya365.mangalampalli@gmail.com>
2016-01-14 05:27:50 +0530

    second commot

---
+++ MVCMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Browse(string genere) {
+            var message = HttpUtility.HtmlEncode("you are looking for"+genere);
+            return message;
+        }
+
+        public string Details(int Id) {
+            return "in Details() id="+Id;
+        }
+    }
+}
